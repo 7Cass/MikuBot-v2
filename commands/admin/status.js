fa03031911acac2e39b42e82003b4eab2d16a349
João Pedro Arruda <jaaopbr@gmail.com>
2021-03-09 21:30:00 -0300

    Feature: Adde status command with only one type

---
+++ commands/admin/status.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "teste",
+  description: "Altera o status do bot",
+  cooldown: 1,
+  require: "ADMINISTRATOR",
+  execute(message, args, client, prefix) {
+    const admin = "476937718677504010" || "501468044234265857";
+    const bot = client.user;
+
+    // const type = parseInt(args.splice(0, 1).join(""));
+    const name = args.map((word) => word).join(" ");
+    console.log(name);
+
+    const presence = {
+      activity: {
+        name,
+        type: 0,
+      },
+    };
+
+    if (message.author.id !== admin) {
+      return message.reply(`Você não tem premissão para usar este comando!`);
+    }
+
+    if (name === "") {
+      return message.reply(
+        `Digite a mensagem desejada como argumento para este comando, ela aparecerá na frente da palavra Jogando! ex: \`${prefix}${this.name} Pedra na sua janela\`  `
+      );
+    }
+    return bot.setPresence(presence);
+  },
+};
